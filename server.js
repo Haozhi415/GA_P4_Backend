48e715b4c79d12b41955e2362ffc304917a69a5c
Haozhi <zhi_0415@hotmail.com>
2024-02-20 22:04:12 +0800

    Replaced http://localhost:3000 with https://ga-p4-backend.onrender.com

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
 // var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user.route.js');
-var authRouter = require('./routes/auth.route.js');
-var listingRouter = require('./routes/listing.route.js');
+var userRouter = require("./routes/user.route.js");
+var authRouter = require("./routes/auth.route.js");
+var listingRouter = require("./routes/listing.route.js");
 
 require("dotenv").config();
 require("./client/mongo");
@@ -16,35 +16,38 @@ require("./client/mongo");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: function (origin, callback) {
-      // allow requests with no origin 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
       // (like mobile apps or curl requests)
       if (!origin) return callback(null, true);
-      
-      const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
+
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://ga-p4-backend.onrender.com",
+      ];
       if (allowedOrigins.indexOf(origin) === -1) {
-          const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-          return callback(new Error(msg), false);
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
       }
       return callback(null, true);
-  },
-  credentials: true
-}));
+    },
+    credentials: true,
+  })
+);
 
-
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
-app.use('/listing', listingRouter);
-
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/listing", listingRouter);
 
 // error handler
 app.use((err, req, res, next) => {

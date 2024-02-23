b1c5dc1cbab3545ffa9372711673531c1f156ac0
Haozhi <zhi_0415@hotmail.com>
2024-02-23 19:57:53 +0800

    added some comment.

--- server.js
+++ server.js
@@ -34,9 +34,11 @@ app.use(
       // Only allows requests from the following origins:
       const allowedOrigins = [
         "https://ga-p4-frontend.onrender.com",
-        // "https://ga-p4-backend.onrender.com",
+        "https://ga-p4-backend.onrender.com",
       ];
       // If a request is made from an origin that is not allowed, return an error message.
+      // Example: If a request is made from origin="http://abc.com", this origin is not inside
+      // the allowedOrigins array, its index will be -1, so the callback will return an error message.
       if (allowedOrigins.indexOf(origin) === -1) {
         const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
         return callback(new Error(msg), false);
